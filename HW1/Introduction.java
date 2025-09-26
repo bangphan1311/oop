beee314df82fceaf3c0430d9cdabe8daeadd1150
unknown <bangphan1311@gmail.com>
2025-09-26 11:27:31 +0700

    Add Introduction.java for HW1

---
+++ HW1/Introduction.java
@@ -0,0 +1,12 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Phan Doãn Thanh Bằng\t24021384\tINT2204 6\tbangphan1311\t24021384@vnu.edu.vn");
+        for (int i = 9; i >= 2; i--) {
+            System.out.println(i + " bottles of beer on the wall, "+ i + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("1 bottle of beer on the wall, 1 bottle of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
